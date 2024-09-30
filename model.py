2e45e9baa8561fab91d9d28ce0ce87628a89c6f4
pavel <pac@yandex.ru>
2024-09-30 15:26:57 +0300

    adding todo

--- model.py
+++ model.py
@@ -1,16 +1,85 @@
-from pydantic import BaseModel
-from typing import List
-class TodoItem(BaseModel):
-    id : int
-    item:str
+from typing import List, Optional  # Импорт типов для аннотаций
+from fastapi import Form  # Импорт для обработки данных формы в FastAPI
+from pydantic import BaseModel  # Импорт базовой модели от Pydantic для валидации данных
+
+
+class Todo(BaseModel):
+    """
+    Модель данных для отдельной задачи (Todo).
+    """
+    id: Optional[int]  # Идентификатор задачи. Опциональный, так как может быть автоматически назначен.
+    item: str  # Описание задачи.
+
+    @classmethod
+    def as_form(
+            cls,
+            item: str = Form(...)
+    ):
+        """
+        Класс-метод для преобразования данных формы в объект модели Todo.
+
+        Args:
+            item (str): Описание задачи, полученное из формы.
+
+        Returns:
+            Todo: Созданный объект модели Todo.
+        """
+        return cls(item=item)
 
     class Config:
+        """
+        Конфигурация модели для Pydantic.
+        """
         schema_extra = {
-            'example': {
-                'id': 1,
-                'item' : "Read more books to become smarter!"
+            "example": {
+                "id": 1,
+                "item": "Example schema!"
             }
         }
 
+
+class TodoItem(BaseModel):
+    """
+    Модель данных для обновления задачи (частичная информация).
+    """
+    item: str  # Обновлённое описание задачи.
+    @classmethod
+    def as_form(
+            cls,
+            item: str = Form(...)
+        ):
+        return cls(item=item)
+
+    class Config:
+        """
+        Конфигурация модели для Pydantic.
+        """
+        schema_extra = {
+            "example": {
+                "item": "Read the next chapter of the book"
+            }
+        }
+
+
 class TodoItems(BaseModel):
-    todos: List[TodoItem]
+    """
+    Модель данных для списка задач.
+    """
+    todos: List[TodoItem]  # Список объектов TodoItem.
+
+    class Config:
+        """
+        Конфигурация модели для Pydantic.
+        """
+        schema_extra = {
+            "example": {
+                "todos": [
+                    {
+                        "item": "Example schema 1!"
+                    },
+                    {
+                        "item": "Example schema 2!"
+                    }
+                ]
+            }
+        }
